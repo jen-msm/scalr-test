cef0d32b0d45303bba9c0b79ac5242d82e05a198
Jenna Ngo <jen.ngo@moneysupermarket.com>
2022-03-24 10:43:15 +0000

    tag fix

--- main.tf
+++ main.tf
@@ -1,14 +1,15 @@
 provider "aws" {
   region     = "eu-west-1"
+  default_tags = {
+      tags = {
+        Name = "Scalr Example"
+        owner = "jen.ngo@moneysupermarket.com"
+    }
+  }
 }
 
 resource "aws_instance" "scalr_instance" {
   ami           = "ami-0069d66985b09d219"
   instance_type = "t2.micro"
-
-  tags = {
-      Name = "Scalr Example"
-      owner = "jen.ngo@moneysupermarket.com"
-  }
 }
  
